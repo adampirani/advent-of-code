7db6bce7122409139b0f2dab5e7b6cff8ce4289e
Adam Pirani <apirani@seatgeek.com>
2024-12-11 19:48:20 -0500

    Clean up a bit

--- 2024/python/day-11/solution/pt2.py
+++ 2024/python/day-11/solution/pt2.py
@@ -24,18 +24,6 @@ def process_blink(rock: int):
         new_list.append(rock*2024)
     return new_list
 
-# print('process_blink([0])', process_blink([0]))
-# print('process_blink([1])', process_blink([1]))
-# print('process_blink([10])', process_blink([10]))
-# print('process_blink([1001])', process_blink([1001]))
-# print('process_blink([99])', process_blink([99]))
-# print('process_blink([999])', process_blink([999]))
-# print('process_blink([999])', process_blink([999]))
-# print('process_blink([1117,0,8,21078,2389032,142881,93,385])', process_blink([1117,0,8,21078,2389032,142881,93,385]))
-
-global num_found_in_dict
-num_found_in_dict = [0]
-
 def count_rock_blink(rock_blink: dict[str, str], count_by_stone_blinks: dict[str, int]):
     rock_val = rock_blink['r']
     num_blinks = rock_blink['blinks']
@@ -43,7 +31,6 @@ def count_rock_blink(rock_blink: dict[str, str], count_by_stone_blinks: dict[str
     key = f"{rock_val}-{num_blinks}"
 
     if key in count_by_stone_blinks:
-        num_found_in_dict[0] +=1
         return count_by_stone_blinks[key]
     
     if num_blinks == 0:
@@ -64,8 +51,6 @@ def count_rock_blink(rock_blink: dict[str, str], count_by_stone_blinks: dict[str
     return sum
     
 
-
-
 def num_stones_by_blinks(rocks: list[int], num_blinks: int):
     count_by_stone_blinks = {}
     ttl_count = 0
@@ -82,39 +67,10 @@ def num_stones_by_blinks(rocks: list[int], num_blinks: int):
     
     return ttl_count
 
-#     l = 0
-#     r = len(blocks) - 1
-#     while l < r:
-#         while blocks[l] != '.':
-#             l+= 1
-#         while blocks[r] == '.':
-#             r-= 1
-
-#         blocks[l] = blocks[r]
-#         blocks[r] = '.'
-#         l+=1
-#         r-=1
-
-# def calc_checksum(blocks: list[int|str]):
-#     total = 0
-
-#     for i in range(len(blocks)):
-#         if blocks[i] == '.':
-#             return total
-#         total+= blocks[i] * i
-
-#     return total
-
-
 # rock_str = read_file('../input/sample.txt')
 rock_str = read_file('../input/full.txt')
 
 rocks = create_rocks(rock_str)
-print('initial rocks: ', rocks)
-
 num_stones = num_stones_by_blinks(rocks, 75)
 
-# processed_rocks = blink_rocks(rocks, 6)
 print(num_stones)
-
-print('num_found_in_dict', num_found_in_dict)

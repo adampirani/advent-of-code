b17411403544e7b8a1dc71e996879addf38b1002
Adam Pirani <apirani@seatgeek.com>
2024-12-24 18:45:50 -0500

    day 20 done

--- 2024/python/day-20/solution/pt2.py
+++ 2024/python/day-20/solution/pt2.py
@@ -69,50 +69,44 @@ def get_path_seconds(track: list[list[str]], start: list[int], end: list[int]):
 
     return visited_points
 
-def get_cheats_by_times(track: list[list[str]], path_seconds: dict[str, int]):
-    max_row = len(track) - 1
-    max_col = len(track[0]) - 1
-
+def get_cheats_by_times(path_seconds: dict[str, int], min_time: int):
     cheats_by_times: dict[int, list[str]] = {}
 
-    for loc_key in path_seconds.keys():
+    loc_keys = list(path_seconds.keys())
+    num_keys = len(loc_keys)
+
+    for i in range(num_keys - min_time): 
+        loc_key = loc_keys[i]
         loc = list(map(int, loc_key.split('_')))
 
-        for increment in DIRECTION_INCREMENTS:
-            first_loc = [loc[0] + increment[0], loc[1] + increment[1]]
-            first_r, first_c = first_loc
+        for j in range(i + min_time, num_keys):
+            new_loc_key = loc_keys[j]            
+            new_loc = list(map(int, new_loc_key.split('_')))
+            time_taken = abs(new_loc[0] - loc[0]) + abs(new_loc[1] - loc[1])
 
-            # to be a cheat, has to jump a wall
-            if track[first_r][first_c] != '#':
+            if time_taken > 20:
                 continue
 
-            new_loc = [loc[0] + increment[0]*2, loc[1] + increment[1]*2]
-            new_loc_key = loc_to_key(new_loc)
+            cheat_val = path_seconds[new_loc_key] - path_seconds[loc_key] - time_taken
+            # print('cheat val: ', cheat_val)
 
-            if new_loc_key not in path_seconds:
-                continue
-
-            r,c = new_loc            
-            if r < 0 or r > max_row or c < 0 or c > max_col:
+            if cheat_val < min_time:
                 continue
 
             cheat_key = f"{loc_key}~{new_loc_key}"
-            cheat_val = path_seconds[new_loc_key] - path_seconds[loc_key] - 2
 
-            if cheat_val > 0:
-                if cheat_val in cheats_by_times:
-                    cheats_by_times[cheat_val].append(cheat_key)
-                else:
-                    cheats_by_times[cheat_val] = [cheat_key]
+            if cheat_val in cheats_by_times:
+                cheats_by_times[cheat_val].append(cheat_key)
+            else:
+                cheats_by_times[cheat_val] = [cheat_key]
     
     return cheats_by_times
 
-def num_cheats_at_least(cheats_by_time: dict[int,list[str]], min_time: int):
+def num_cheats(cheats_by_time: dict[int,list[str]]):
     count = 0
 
     for key in cheats_by_time.keys():
-        if key >= min_time:
-            count+= len(cheats_by_time[key])
+        count+= len(cheats_by_time[key])
         # print(f"there are {len(cheats_by_time[key])} cheats that save {key} seconds")
     return count
 
@@ -122,8 +116,9 @@ race_track, start, end = read_file_two_d_array('../input/full.txt')
 path_seconds = get_path_seconds(race_track, start, end)
 # print('path seconds', path_seconds)
 
-cheats_by_time = get_cheats_by_times(race_track, path_seconds)
-count = num_cheats_at_least(cheats_by_time, 100)
+# cheats_by_time = get_cheats_by_times(path_seconds, 50)
+cheats_by_time = get_cheats_by_times(path_seconds, 100)
+count = num_cheats(cheats_by_time)
 
 print('count: ', count)
 
